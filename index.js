95945a0098b79c7b99a9cfa98fa8f0e33cc738d8
rajesh2285 <mukkapatirajesh9@gmail.com>
2018-04-11 17:14:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!This is Testing Deployment on Azure instance Through Travis Ci");
+    response.end("Hello World!This is Testing Deployment on Azure instance Through Travis Ci\n\nThank You!");
   
 
 });
