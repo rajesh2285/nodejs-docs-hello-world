dede41a3d60f075684c62be85e201351a2b6c6f9
rajesh2285 <mukkapatirajesh9@gmail.com>
2018-04-11 17:13:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var http = require('http');
-var fs = require("fs");
-var data = fs.readFileSync('process.json');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!This is Testing Deployment on Azure instance Through Travis Ci");
-   
-console.log(data.toString());
-console.log("Program Ended");
+  
 
 });
 
